d6442ecb376361a2b60ef29efb50e1845f4f7643
Mehroj Islomov <mehroj134@gmail.com>
2025-09-24 17:07:36 +0300

    Add all

---
+++ test2.py
@@ -0,0 +1,20 @@
+# app.py
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_xDJIahtAIeCeYI96swfOSBAF1gax8tVhlOVABilc"
+    response = requests.get(url)
+    tjs_value = "Не удалось загрузить"
+
+    if response.status_code == 200:
+        data = response.json()
+        tjs_value = data['data']['TJS']['value']
+
+    return render_template('index.html', tjs_value=tjs_value)
+
+if __name__ == "__main__":
+    app.run(debug=True)
